33057658db61f61ae9ce037821c2e74c0f3e76ec
Rian Sanjaya <rian.sj@gmail.com>
2019-02-11 08:40:51 +0700

    Initial Commit

---
+++ onlineStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  onlineStore
+//
+//  Created by Rian Sanjaya Ng on 11/02/19.
+//  Copyright © 2019 Rian Sanjaya Ng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
